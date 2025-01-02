27b3811d33ad299f3e2e2104d8e2bb81bb450dfa
𝑴𝑼𝑹𝑨𝑳𝑰 <148250637+Itz-Murali@users.noreply.github.com>
2025-01-02 12:51:23 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,14 +1,40 @@
+#
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
+commands = {}
+
+
 languages_present = {}
+
+
+def get_command(value: str) -> List:
+    return commands["command"][value]
 
 
 def get_string(lang: str):
     return languages[lang]
+
+
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(r"./strings/" + filename, encoding="utf8")
+        )
 
 
 for filename in os.listdir(r"./strings/langs/"):
@@ -30,5 +56,7 @@ for filename in os.listdir(r"./strings/langs/"):
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
+        )
+        sys.exit()
