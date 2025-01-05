8a9ad0134803c418bddf5be2a1fbf34138492955
𝑴𝑼𝑹𝑨𝑳𝑰 <148250637+Itz-Murali@users.noreply.github.com>
2025-01-05 14:27:56 +0530

    Add files via upload

---
+++ Yukki.py
@@ -0,0 +1,19 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+Owner = list(map(int, getenv("Owner", "6294902190 7301077117 6764358144").split()))
+
+
+
+def small_caps(text):
+    small_caps_map = str.maketrans(
+        "abcdefghijklmnopqrstuvwxyz",
+        "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
+    )
+    return text.translate(small_caps_map)
+  
