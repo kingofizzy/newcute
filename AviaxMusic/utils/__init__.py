df492e7b3c0f66e41414b7bfe5235bf96d908a3c
𝑴𝑼𝑹𝑨𝑳𝑰 <148250637+Itz-Murali@users.noreply.github.com>
2025-01-05 16:59:54 +0530

    Update __init__.py

--- AviaxMusic/utils/__init__.py
+++ AviaxMusic/utils/__init__.py
@@ -6,3 +6,30 @@ from .formatters import *
 from .inline import *
 from .pastebin import *
 from .sys import *
+
+
+
+ZeroTwo = [
+    "Zero1",
+    "Zero2",
+    "Zero3",
+    "Zero4",
+    "Zero5",
+    "Zero6",
+    "Zero7",
+    "Zero8",
+    "Zero9",
+    "Zero10",
+    "Zero11",
+    "Zero12",
+    "Zero13",
+    "Zero14",
+    "Zero15",
+    "Zero16",
+    "Zero17",
+    "Zero18",
+    "Zero19",
+    "Zero20",
+    "Zero21",
+    "Zero22",
+]
