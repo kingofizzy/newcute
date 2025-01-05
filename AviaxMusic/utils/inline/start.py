d18e8d37f0451831a2b936b7869d14b317a2256d
𝑴𝑼𝑹𝑨𝑳𝑰 <148250637+Itz-Murali@users.noreply.github.com>
2025-01-05 18:15:42 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -16,22 +16,37 @@ def start_panel(_):
     return buttons
 
 
-def private_panel(_):
+
+def private_panel(OWNER: Union[bool, int] = None):
     buttons = [
+       [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users",
+            ),
+        ], 
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+                text="Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅs", callback_data="gotohelp"
+            ),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+      [
+            InlineKeyboardButton(
+               text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", 
+               user_id=OWNER,
+                    ),
+          InlineKeyboardButton(
+               text="ᴏᴡɴᴇʀ", 
+               url=f"https://t.me/itzYuva",
+                    ),
+      ],
+          [
+           InlineKeyboardButton(
+               text="sᴜᴘᴘᴏʀᴛ",
+               url=f"https://t.me/Sigma_Rulez"
+           ),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
-        ],
-    ]
+    
+]
     return buttons
+
